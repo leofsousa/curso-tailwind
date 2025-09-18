b4af7fc978637b731b0ca54827344051323bb588
Leozera2k <leofsousa98@gmail.com>
2025-09-18 13:34:00 -0300

    aula de espaçamentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 function App() {
   return (
     <>
-      <body className="h-screen bg-gradient-to-r from-amber-700 to-slate-600 ">
-        <h1 className="text-3xl text-center text-red-700 bg-gray-500 p-2">
-          Welcome to Vite with TailwindCSS and React
-        </h1>
-      </body>
+      <div className="flex justify-center">
+        <div className="flex flex-col space-y-4 h-96 w-5/6 bg-indigo-700 border-4 border-red-400 items-center justify-center">
+          <div className="h-20 w-20 bg-blue-400 border-4 border-orange-400">
+            1
+          </div>
+          <div className="h-20 w-20 bg-green-400 border-4 border-orange-400">
+            2
+          </div>
+          <div className="h-20 w-20 bg-yellow-400 border-4 border-orange-400">
+            3
+          </div>
+        </div>
+      </div>
     </>
   );
 }
