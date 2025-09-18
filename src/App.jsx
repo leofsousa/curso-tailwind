2a472c8c95a572387e68e8c5bf60ef5289585119
Leozera2k <leofsousa98@gmail.com>
2025-09-18 13:15:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
-
 function App() {
-
   return (
     <>
-      <h1
-        className="text-3xl text-center text-red-700"
-      >Welcome to Vite with TailwindCSS and React</h1>
+      <body className="h-screen bg-gradient-to-r from-amber-700 to-slate-600 ">
+        <h1 className="text-3xl text-center text-red-700 bg-gray-500 p-2">
+          Welcome to Vite with TailwindCSS and React
+        </h1>
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
