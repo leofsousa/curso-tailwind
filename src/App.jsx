a16cc684338445a7afc75198007b688ca2fbd002
Leozera2k <leofsousa98@gmail.com>
2025-09-18 13:39:59 -0300

    preparando para início do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 function App() {
   return (
     <>
-      <div className="flex justify-center">
-        <div className="flex flex-col space-y-4 h-96 w-5/6 bg-indigo-700 border-4 border-red-400 items-center justify-center">
-          <div className="h-20 w-20 bg-blue-400 border-4 border-orange-400">
-            1
-          </div>
-          <div className="h-20 w-20 bg-green-400 border-4 border-orange-400">
-            2
-          </div>
-          <div className="h-20 w-20 bg-yellow-400 border-4 border-orange-400">
-            3
-          </div>
-        </div>
-      </div>
+      
     </>
   );
 }
